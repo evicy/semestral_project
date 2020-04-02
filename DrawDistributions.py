070476ba77c4fca76eb00849d2c25936e487d34c
evi <herencsareva@gmail.com>
2020-04-03 00:08:10 +0200

    Using fasta files, changed visualizations, removed bugs

--- DrawDistributions.py
+++ DrawDistributions.py
@@ -32,6 +32,7 @@ def draw(histo, max_x = 30, num_of_shifts = 4):
     #distributions.append(dTuple.DistributionTuple(disc.Discretize(st.cauchy.cdf), [0.0001], 'Cauchy', 'fuchsia'))
     distributions.append(dTuple.DistributionTuple(disc.Discretize(st.norm.cdf), [20, 1], 'Gauss', 'black'))
     #distributions.append(dTuple.DistributionTuple(st.betabinom, [len(x), 1, 1], 'BetaBinom', 'black'))
+
     #pole objektov DistributionData
     data = []
 
@@ -45,7 +46,10 @@ def draw(histo, max_x = 30, num_of_shifts = 4):
             tail = tailer.Tailer(distributions[j].distribution, y, i)
             popt, pcov = optimize.curve_fit(tail.pmf, xdata=x, ydata=y, p0=distributions[j].p0)
             xspace = np.linspace(1, x[len(x)-1], num=x[len(x)-1])
-            plt.plot(xspace, tail.pmf(xspace,*popt), color=distributions[j].color, linewidth=2, label=distributions[j].name)
+            if(j % 2 == 1):
+                plt.plot(xspace, tail.pmf(xspace,*popt), linestyle='dashed', color=distributions[j].color, linewidth=2, label=distributions[j].name)
+            else:
+                plt.plot(xspace, tail.pmf(xspace,*popt), color=distributions[j].color, linewidth=2, label=distributions[j].name)
             a = tail.pmf(xspace,*popt)
             l2_error = LA.norm(a[:len(y)] - y[:len(y)])
             l1_error = LA.norm(a[:len(y)] - y[:len(y)], ord=1)
@@ -53,9 +57,9 @@ def draw(histo, max_x = 30, num_of_shifts = 4):
         
         plt.ylim(min(y), 2)
         plt.xlim(0, max_x)
-        plt.title('Analyza frekvencii k-merov, shiftnute o ' + str(i), fontsize=20)
-        plt.xlabel('Nasobnost k-meru', fontsize=15)
-        plt.ylabel('Pocet k-merov', fontsize=15)
+        plt.title('Histogram o početnosti čítaní', fontsize=20)
+        plt.xlabel('Čítania na intervaloch', fontsize=15)
+        plt.ylabel('Pocet čítaní', fontsize=15)
         plt.legend(loc='best', prop={'size': 15})
         plt.show()
 
@@ -63,23 +67,23 @@ def draw(histo, max_x = 30, num_of_shifts = 4):
     #    print(data[i].l1_error)
 
     # usporiadame podla L1 normy
-    data.sort(key=lambda x: x.l1_error)
+    #data.sort(key=lambda x: x.l1_error)
     
     # vypis 3 najlepsich distribucii podla L1 normy
-    print("Top 3 for L1 norm:")
-    print(distributions[data[0].distribution_num].name, ", shifted: ", data[0].shift)
-    print(distributions[data[1].distribution_num].name, ", shifted: ", data[1].shift)
-    print(distributions[data[2].distribution_num].name, ", shifted: ", data[2].shift)
-    print()
+    #print("Top 3 for L1 norm:")
+    #print(distributions[data[0].distribution_num].name, ", shifted: ", data[0].shift)
+    #print(distributions[data[1].distribution_num].name, ", shifted: ", data[1].shift)
+    #print(distributions[data[2].distribution_num].name, ", shifted: ", data[2].shift)
+    #print()
 
     # usporiadame podla L2 normy
-    data.sort(key=lambda x: x.l2_error)
+    #data.sort(key=lambda x: x.l2_error)
 
     # vypis 3 najlepsich distribucii podla L2 normy
-    print("Top 3 for L2 norm:")
-    print(distributions[data[0].distribution_num].name, ", shifted: ", data[0].shift)
-    print(distributions[data[1].distribution_num].name, ", shifted: ", data[1].shift)
-    print(distributions[data[2].distribution_num].name, ", shifted: ", data[2].shift)
+    #print("Top 3 for L2 norm:")
+    #print(distributions[data[0].distribution_num].name, ", shifted: ", data[0].shift)
+    #print(distributions[data[1].distribution_num].name, ", shifted: ", data[1].shift)
+    #print(distributions[data[2].distribution_num].name, ", shifted: ", data[2].shift)
 
     # vratime data, ktoru potom pouzije funkcia DrawErrors
     data_for_drawing = DataForDrawing.DataForDrawing(data, num_of_shifts, distributions)
